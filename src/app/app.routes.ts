69eaeadbebfa9ac86d981415580e271c62fe55a0
pcasano <pablo.casanovas@pm.me>
2026-07-06 21:19:04 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CalendarPage} from './calendar-page/calendar-page';
+import {SettingsPage} from './settings-page/settings-page';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calendar', component: CalendarPage },
+  { path: 'settings', component: SettingsPage }
+
+];
