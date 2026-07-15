b15172da0256e235e3eacb7d06bee5b7995dea61
pcasano <pablo.casanovas@pm.me>
2026-07-15 20:24:49 +0200

    angular material installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {CalendarPage} from './calendar-page/calendar-page';
 import {SettingsPage} from './settings-page/settings-page';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'calendar', component: CalendarPage },
   { path: 'settings', component: SettingsPage }
 
