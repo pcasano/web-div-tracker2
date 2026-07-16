021d442e3068b2234d08940c1609d439ef92d971
pcasano <pablo.casanovas@pm.me>
2026-07-16 23:54:55 +0200

    add form

--- src/app/settings-page/settings-page.ts
+++ src/app/settings-page/settings-page.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './settings-page.html',
-  styleUrl: './settings-page.css',
+  styleUrl: './settings-page.css'
 })
-export class SettingsPage {}
+export class SettingsPage {
+
+  readonly form = new FormGroup({
+    baseUrl: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    token: new FormControl('', {
+      validators: [Validators.required]
+    })
+  });
+
+  save(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.getRawValue());
+  }
+}
